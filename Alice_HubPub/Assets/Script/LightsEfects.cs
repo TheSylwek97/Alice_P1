a543637bf660e3cc05beef9075862f1188248849
TheSylwek97 <sylwia97.28@gmail.com>
2020-01-18 20:07:28 +0100

    Ruch kamery na 1. sceny

---
+++ Alice_HubPub/Assets/Script/LightsEfects.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsEfects : MonoBehaviour
+{
+    //public Light Light;
+    public bool isLightOff;
+    public GameObject gameObject;
+   // public bool isEmissionOn;
+    void Start()
+    {
+        
+    }
+    void TurnLight()
+    {
+        if (!isLightOff)
+        {
+            //Light.enabled = true;
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            //Light.enabled = false;
+            gameObject.SetActive(false);
+        }
+    }
+    /*
+    void TurnEmission()
+    {
+        if (isLightOn)
+            Light.enabled = true;
+        else
+            Light.enabled = false;
+    }*/
+   
+}
