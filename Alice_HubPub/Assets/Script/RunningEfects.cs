c450c81002c29c67c2ffcb209d1c6aabbe73ab4f
TheSylwek97 <sylwia97.28@gmail.com>
2020-01-18 20:18:31 +0100

    Nie udana proba

---
+++ Alice_HubPub/Assets/Script/RunningEfects.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningEfects : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void PositionChange()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+}
