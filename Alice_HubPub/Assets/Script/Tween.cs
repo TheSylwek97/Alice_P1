3ed939bc63a11ae802a46fe9b76e579c8413d8a9
Włosek Klaudia <klaudia.wlosek@microsoft.wsei.edu.pl>
2020-01-24 09:40:06 +0100

    Tween nowy

---
+++ Alice_HubPub/Assets/Script/Tween.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tween : MonoBehaviour
+{
+    public iTween.EaseType easeType;
+    public iTween.LoopType loopType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        iTween.RotateTo(this.gameObject, iTween.Hash("x", 5, "easyType", easeType, "time", 0.5f, "loopType", loopType));
+    }
+}
